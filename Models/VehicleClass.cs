2b7f374bc2c6fc32bd5150e6439b92c68a59fb29
Shruti Palekar <palekarshruti1@gmail.com>
2025-12-03 09:49:52 +0530

    Add properties to VehicleClass for vehicle
    Added properties for vehicle details including Id, Name, type, brand, model, price per hour, price per day, fuel type, transmission, and status.

--- Models/VehicleClass.cs
+++ Models/VehicleClass.cs
@@ -2,5 +2,22 @@
 {
     public class VehicleClass
     {
+         public int Id { get; set; } 
+        public string Name { get; set; }
+        public string type { get; set; }
+
+        public string brand { get; set; }
+
+        public string model { get; set;}
+
+        public string priceperhour { get; set; }
+
+        public string priceperday { get; set;}
+
+        public string fueltype { get; set;}
+
+        public string transmission { get; set;}
+
+        public string status { get; set;}
     }
 }
