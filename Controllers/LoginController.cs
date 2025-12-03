8ab4ab9f3487630ec6001620da65c0ea6490e8fd
Shruti Palekar <palekarshruti1@gmail.com>
2025-12-03 09:49:05 +0530

    this the change of the add the endpoint

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -14,7 +14,60 @@ namespace rentcarbike.Controllers
         {
             _connectionString = connectionString;
         }
+        [HttpGet("get-all-the-user-deatils")]
+        public ActionResult<List<UsersClass>> GetAllSignups()
+        {
+            var result = _database.GetAllSignups();
+            return Ok(result);
+        }
+        [HttpGet("get-username-password")]
+        public ActionResult<List<UsersClass>> GetAllUserPass()
+        {
+            var result = _database.GetAllUserPass();
+            return Ok(result);
+        }
 
+        [HttpPost("insert-the-user-deatils")]
+        public IActionResult Insertinuser([FromBody] UsersClass usersclass)
+        {
+            try
+            {
+                _database.InsertSignup(signup);
+                return Ok("insert the data");
+            }
+            catch (Exception ex)
+            {
+                // Log exception (if logging is set up)
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+//all endpoint of vehicle table 
+        [HttpGet("get-all-Vehicle")]
+        public ActionResult<List<VehicleClass>> GetVehicles()
+        {
+            var result = _database.GetVehicles();
+            return Ok(result);
+        }
+
+        [HttpPost("add-Vehicle")]
+        public IActionResult InsertEmployeeWithLogin([FromBody] VehicleClass vehicle)
+        {
+            if (vehicle == null)
+            {
+                return BadRequest("Employee data is required.");
+            }
+
+            try
+            {
+                _database.InsertVehicle(vehicle);
+                return Ok("Employee and login inserted successfully.");
+            }
+            catch (Exception ex)
+            {
+                // Log exception (if logging is set up)
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
         
     
     }
