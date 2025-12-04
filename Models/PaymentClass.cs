484c85b15d361a92f9be01529b999deb17b4027a
Shruti Palekar <palekarshruti1@gmail.com>
2025-12-04 10:31:51 +0530

    Add properties to PaymentClass for payment details

--- Models/PaymentClass.cs
+++ Models/PaymentClass.cs
@@ -2,5 +2,10 @@
 {
     public class PaymentClass
     {
+        public int PaymentId { get; set; }
+    public int BookingId { get; set; }
+    public string PaymentMethod { get; set; }
+    public string Status { get; set; }
+    public DateTime PaymentDate { get; set; }
     }
 }
