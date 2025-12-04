186a39438e50e9918da75e6884d0235325036acd
Shruti Palekar <palekarshruti1@gmail.com>
2025-12-04 10:35:52 +0530

    Add properties to VehicleImagesClass

--- Models/VehicleImagesClass.cs
+++ Models/VehicleImagesClass.cs
@@ -1,6 +1,15 @@
-﻿namespace rentcarbike.Models
+
+namespace rentcarbike.Models
 {
     public class VehicleImagesClass
     {
+
+         public int ImageId { get; set; }
+    public int VehicleId { get; set; }
+
+    // VARBINARY → byte[]
+    public byte[] ImageUrl { get; set; }
+
+    public string VehicleName { get; set; }
     }
 }
