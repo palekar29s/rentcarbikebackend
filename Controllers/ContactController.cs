9a5325427253ce8bd9eaf51cd6c5560a75df87ee
sandeshsynoris <sandesh.palekar@synoris.co.in>
2026-02-16 22:37:51 +0530

    Backend project update

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using rentcarbike.Models;
+
+namespace rentcarbike.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+
+        private readonly Database _database;
+        public ContactController(Database database)
+        {
+            _database = database;
+        }
+
+        [HttpPost]
+        public IActionResult AddContact([FromBody] Contact contact)
+        {
+            _database.AddContact(contact.Name, contact.Email, contact.Phone, contact.Message);
+
+            return Ok(new { message = "Message submitted successfully" });
+        }
+        [HttpGet]
+        public IActionResult GetAllContacts()
+        {
+            var contacts = _database.GetAllContacts();
+            return Ok(contacts);
+        }
+
+    }
+}
---
+++ Models/vehicleeClass.cs
@@ -0,0 +1,24 @@
+﻿namespace rentcarbike.Models
+{
+    public class vehicleeClass
+    {
+        public int VehicleId { get; set; }
+        public string Name { get; set; }
+        public string type { get; set; }
+        public string brand { get; set; }
+        public string model { get; set; }
+        public decimal priceperhour { get; set; }
+        public decimal priceperday { get; set; }
+        public string fueltype { get; set; }
+        public string transmission { get; set; }
+        public string imageUrl { get; set; }
+
+        public string status { get; set; }
+
+       
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string Location { get; set; }
+        public int? Seats { get; set; }
+    }
+}
